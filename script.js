c693c1b3ee6c6eb7a9a238c3390ec2a71c029750
Newton Lomar <newtonlomar16@gmail.com>
2021-10-11 14:17:15 -0300

    improving the fetch code

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const array = []
+const arrayOfCities = []
 
-const getData = async () => {
-  await fetch(endpoint).then(res => res.json()).then((data) => {
-    data.forEach((item) => {
-      array.push(item)
-    })
+fetch(endpoint)
+.then(res => res.json())
+.then((data) => {
+  data.forEach((city) => {
+    arrayOfCities.push(city)
   })
-}
+})
 
-getData()
 
 // Ideia do filtro, usar algo do tipo "inclusdes" para o array
